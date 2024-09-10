52aab568a2ae406df28daceb7e1c156ba045affc
DiegoMetzler <metzlerdiego@gmail.com>
2024-09-09 22:08:57 -0300

    inicio de proyecto

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
